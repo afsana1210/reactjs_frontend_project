7308977dedbe9aae121065495f9f6ad1cae78fa8
afsana1210 <afsanaansari1210@gmail.com>
2024-08-26 17:44:24 +0530

    Context provider added

--- github-finder/src/App.js
+++ github-finder/src/App.js
@@ -4,23 +4,26 @@ import Footer from "./components/layout/Footer";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import NotFount from "./pages/NotFount";
+import { GithubProvider } from "./context/github/GithubContext";
 function App() {
   return (
-    <Router>
-      <div className="flex flex-col justify-between h-screen">
-        <Navbar />
+    <GithubProvider>
+      <Router>
+        <div className="flex flex-col justify-between h-screen">
+          <Navbar />
 
-        <main className="container mx-auto px-3 pb-12">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/notfound" element={<NotFount />} />
-            <Route path="/*" element={<NotFount />} />
-          </Routes>
-        </main>
-        <Footer />
-      </div>
-    </Router>
+          <main className="container mx-auto px-3 pb-12">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/notfound" element={<NotFount />} />
+              <Route path="/*" element={<NotFount />} />
+            </Routes>
+          </main>
+          <Footer />
+        </div>
+      </Router>
+    </GithubProvider>
   );
 }
 
