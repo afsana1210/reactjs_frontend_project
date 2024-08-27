cb3c82512dcff61d1c2163d8ffb3da94ae100eec
afsana1210 <afsanaansari1210@gmail.com>
2024-08-27 21:23:48 +0530

    Reducer function new cases added to clear user state after search

--- github-finder/src/context/github/GithubReducer.jsx
+++ github-finder/src/context/github/GithubReducer.jsx
@@ -11,6 +11,11 @@ const githubRducer = (state, action) => {
         ...state,
         loading: true,
       };
+    case "CLEAR_USERS":
+      return {
+        ...state,
+        users: [],
+      };
     default:
       return state;
   }
