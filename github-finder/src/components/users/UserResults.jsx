94054130c78348e2c2089a2227d4a51e957c73b2
afsana1210 <afsanaansari1210@gmail.com>
2024-08-29 18:16:52 +0530

    added user search function with context state

--- github-finder/src/components/users/UserResults.jsx
+++ github-finder/src/components/users/UserResults.jsx
@@ -1,26 +1,15 @@
-import React, { useEffect, useState } from "react";
+import React, { useContext } from "react";
 import Spinner from "../layout/Spinner";
 import UserItem from "./UserItem";
-const REACT_API_GITHUB_URL = "https://api.github.com";
+import GithubContext from "../../context/github/GithubContext";
 
-function UserResults() {
-  const [users, setUsers] = useState();
-  const [loading, setLoading] = useState(true);
+const UserResults = () => {
+  const { users, loading } = useContext(GithubContext);
 
-  useEffect(() => {
-    fetchUsers();
-  }, []);
-
-  const fetchUsers = async () => {
-    const response = await fetch(`${REACT_API_GITHUB_URL}/users`);
-    const data = await response.json();
-    setUsers(data);
-    setLoading(false);
-  };
   if (!loading) {
     return (
       <div className="grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2">
-        {users.map((user) => (
+        {users?.map((user) => (
           <UserItem key={user.id} user={user} />
         ))}
       </div>
@@ -28,6 +17,6 @@ function UserResults() {
   } else {
     return <Spinner />;
   }
-}
+};
 
 export default UserResults;
