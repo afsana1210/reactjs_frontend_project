2c2b22ce34ec513d6c1f38923d44d76f95f3a3ff
afsana1210 <afsanaansari1210@gmail.com>
2024-09-01 21:45:21 +0530

    single user route added

--- github-finder/src/App.js
+++ github-finder/src/App.js
@@ -4,6 +4,7 @@ import Footer from "./components/layout/Footer";
 import Alert from "./components/layout/Alert";
 import Home from "./pages/Home";
 import About from "./pages/About";
+import User from "./pages/User";
 import NotFount from "./pages/NotFount";
 import { GithubProvider } from "./context/github/GithubContext";
 import { AlertProvider } from "./context/alert/AlertContext";
@@ -20,6 +21,7 @@ function App() {
               <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/about" element={<About />} />
+                <Route path="/user/:login" element={<User />} />
                 <Route path="/notfound" element={<NotFount />} />
                 <Route path="/*" element={<NotFount />} />
               </Routes>
